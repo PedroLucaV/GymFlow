a32b4f3290a9edc4de5d2b7359fc8e24960a2963
Pedro Lucas <133527072+PedroLucaV@users.noreply.github.com>
2026-03-27 08:56:48 -0300

    Revert "Adding redis"

--- app/Features/Users/Routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Features\Users\Controllers\UserController;
-
-Route::prefix('users')->group(function () {
-    Route::post('/register', [UserController::class, 'store']);
-});
